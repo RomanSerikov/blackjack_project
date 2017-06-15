d72b227a2e16337b670c8214afcf146327bc0bb8
Roman Serikov <roman.serikov@yandex.ru>
2017-06-15 22:42:12 +0300

    Fill Deck Class

--- deck.rb
+++ deck.rb
@@ -1,12 +1,29 @@
 class Deck
-  @cards = %w(2 3 4 5 6 7 8 9 10 J Q K A)
-  @suits = %w(+ <3 ^ <>)
-
   def initialize
-    
+    @cards = []
+    @values = %w(2 3 4 5 6 7 8 9 10 J Q K A)
+    @suits  = %w(+ <3 ^ <>)
+    @points = {"A" => "ACE", "J" => 10, "Q" => 10, "K" => 10}
+    make_deck
+    shuffle
   end
 
+  def take_card
+    @cards.shift
+  end
+
+  private
+  
   def make_deck
-    
+    @values.each do |value|
+      @suits.each do |suit|
+        points = @points.has_key?(value) ? @points[value] : value.to_i
+        @cards << Card.new(value: value, suit: suit, points: points)
+      end
+    end
+  end
+
+  def shuffle
+    @cards.shuffle!
   end
 end
